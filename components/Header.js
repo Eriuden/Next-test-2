30db975435465a91d1774a593c9b8d95417eb626
Eriuden <antoinecarrette@hotmail.fr>
2023-04-11 20:04:26 +0200

    pursue auth

--- components/Header.js
+++ components/Header.js
@@ -2,13 +2,18 @@ import { Signika_Negative } from 'next/font/google'
 import Link from 'next/link'
 import React from 'react'
 // import signIn signOut from "next-auth/client"
+// import useSession
 // Oui, il y a des fonctions co et déco déjà toutes prêtes dans next !
+// useSession marche ici pour vérifier si l'user est co, très pratique !
 
 export const Header = () => {
+  const [session, loading] = useSession()
   return (
     <div className='layout-header'>
-      <ul>
-        <li>
+      <ul className={`main-nav ${!session && loading ? `loading` : `loaded`}`}>
+
+        {!loading && !session && (
+          <li>
           <Link href="/api/auth/signin">
             <a onClick={e => {
               e.preventDefault()
@@ -16,15 +21,20 @@ export const Header = () => {
             }}>Connexion</a>
           </Link>
         </li>
+        )}
+      
+        {session && (
+          <li>
+            <Link href="/api/auth/signout">
+              <a onClick={e => {
+                e.preventDefault()
+                SignOut()
+              }}>Déconnexion</a>
+            </Link>
+          </li>
+        )}
 
-        <li>
-          <Link href="/api/auth/signout">
-            <a onClick={e => {
-              e.preventDefault()
-              SignOut()
-            }}>Déconnexion</a>
-          </Link>
-        </li>
+        
       </ul>
     </div>
   )
--- pages/api/dashboardApi.js
+++ pages/api/dashboardApi.js
@@ -1,3 +1,33 @@
-export const handler = (req, res) => {
-    res.status(200).json({ name: "Dashboard API route"})
+//import getSession
+//getSession renvoie une promesse nulle si il n'y a pas de session
+/*
+    UseEffect va donc afficher selon le résultat de getSession, si rien
+    Il nous envoie nous connecter, sinon, on stoppe le loading
+    Enfin, on appelle securePage pour que UseEffect puisse la lancer
+*/ 
+import { useState,useEffect } from "react"
+
+export const dashboardApi = () => {
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const securePage = async() => {
+            const session = await getSession()
+            if(!session) {
+                SignIn()
+            } else {
+                setLoading(false)
+            }
+        }
+        securePage()
+    }, [])
+
+    if (loading) {}
+
+  return (
+    <div>
+        <h1>Dashboard</h1>
+    </div>
+  )
 }
+
